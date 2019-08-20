8961736e1570aa7f799f5e2fb58682cd0a94a2a1
Erik Chagas <erikchagas00@gmail.com>
2019-08-20 01:26:50 -0400

    Add post method to insert a new project

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({
+    id,
+    title,
+    tasks: []
+  });
+
+  return res.json(projects);
+});
+
 //http://localhost:3333/
 server.listen(3333);
